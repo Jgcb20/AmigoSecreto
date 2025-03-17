501cab6bda3fda7b2a1356d6ed9a8ad36887dfbf
Jgcb20 <jgcb20@gmail.com>
2025-03-17 17:04:42 -0600

    última version del proyecto y se agrego la funcón para que a la hora de sortear a los amigos secretos no se vea la lista

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ function capturaAmigos() {
 
     // Limpiar el campo de entrada
     input.value = ""; 
+
+    //Llama a la función que va listando los amigos que se escriben
     mostrarAmigos();
     }
 
@@ -27,11 +29,39 @@ function capturaAmigos() {
 // Función para mostrar la lista de amigos
 function mostrarAmigos() {
     let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // Limpiar lista antes de actualizar
+
+    // Limpiar lista antes de actualizar
+    lista.innerHTML = ""; 
    
+    //Va mostrando la lista generada
     for (let amigo of amigos) {
         let li = document.createElement("li");
         li.textContent = amigo;
         lista.appendChild(li);
     }
+}
+
+// Función para seleccionar un amigo aleatoriamente
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+   
+    let seleccionarAmigoAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSeleccionado = amigos[seleccionarAmigoAleatorio];
+    document.getElementById("resultado").innerHTML = `Amigo sorteado: <strong>${amigoSeleccionado}</strong>`;
+
+    //Llama a la funcón 
+    noMostrarLista();
+}
+
+
+//Función para que no aparezca la lista de amigos mientras se sortea
+function noMostrarLista(){
+    let vaciarlista = document.getElementById("listaAmigos");
+
+    // Limpiar lista antes de actualizar
+    vaciarlista.innerHTML = ""; 
+
 }
