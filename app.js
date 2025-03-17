db3c3ef4a42907b02ba042fa09e4ea6f8edf11c8
Jgcb20 <jgcb20@gmail.com>
2025-03-16 20:03:12 -0600

    Subida de proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variable tipo lista ce almacena a los amigos ingresados
+let amigos = [];
+
+//Funcion para hacer la captura de los amigos 
+function capturaAmigos(){
+
+
+}
